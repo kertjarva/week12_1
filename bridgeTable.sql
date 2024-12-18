bec5e49e1edd53b340862bd646b29e3bb266094b
Kert <kert.jarva@tktk.ee>
2024-12-18 16:06:04 +0200

    week 11

---
+++ bridgeTable.sql
@@ -0,0 +1,75 @@
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 1);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 2);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 3);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 4);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 5);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 6);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (1, 7);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (2, 9);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (2, 3);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (2, 4);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (2, 10);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (2, 11);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 1);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 12);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 5);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 14);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 15);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 16);
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+VALUES (3, 17);
+
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+SELECT a.id, b.id 
+FROM recipe a
+JOIN ingredient b
+ON a.recipeName = 'Pumpkin Tartlets' AND b.ingredientname = 'pumpkin puree';
+
+INSERT INTO ingredientinrecipe (recipeid, ingredientid)
+SELECT a.id, b.id 
+FROM recipe a
+JOIN ingredient b
+ON a.recipeName = 'Pumpkin Tartlets' AND b.ingredientname = 'brown sugar';
+
+SELECT a.recipeName, b.ingredientName FROM recipe a
+INNER JOIN IngredientInRecipe c
+ON a.id = c.recipeId
+INNER JOIN ingredient b
+ON b.id = c.ingredientId;
---
+++ routes/randomRecipe.routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const db = require('../db'); 
+const router = express.Router(); 
+
+router.get('/', async (req, res) => {
+
+    try {
+        const recipeQuery = 'SELECT id, recipeName, instructions FROM recipe ORDER BY RANDOM() LIMIT 1;';
+    const recipeResult = await db.query(recipeQuery);
+    const selectedRecipe = recipeResult.rows[0];
+    const ingredientsQuery = 'SELECT b.ingredientName FROM ingredient b INNER JOIN IngredientInRecipe c ON b.id = c.ingredientId WHERE c.recipeId = $1;';
+    const ingredientResult = await db.query(ingredientsQuery, [selectedRecipe.id]);
+    const ingredients = ingredientResult.rows.map( element => element.ingredientname);
+   
+
+    const randomRecipes = {
+        recipe: selectedRecipe,
+        ingredients:ingredients
+    };
+    res.json(randomRecipes);
+    }
+
+    catch(error) {
+        console.log(error);
+        res.status(500).json({error:'Internal server error.'});
+    }
+
+}); 
+
+module.exports = router;
